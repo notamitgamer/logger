250b3081cf76bf4103c0f704c4ad92b490f41ccd
Amit Dutta <mail@amit.is-a.dev>
2026-08-25 07:28:18 +0530

    Update browser version in WASocket configuration

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ async function startWhatsApp() {
         version,
         logger,
         auth: state,
-        browser: ["WhatsApp Logger Backend", "Chrome", "1.0.0"],
+        browser: ["WhatsApp Logger v4.2.1", "Chrome", "4.2.1"],
         syncFullHistory: true 
     });
 
